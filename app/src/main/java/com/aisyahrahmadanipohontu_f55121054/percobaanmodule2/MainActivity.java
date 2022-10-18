54fdb30e137ec12f8e5078e4a6e80f87e659d2a9
Putri Innayah <jy.putriii@gmail.com>
2022-10-18 19:32:40 +0700

    Initial commit

---
+++ app/src/main/java/com/aisyahrahmadanipohontu_f55121054/percobaanmodule2/MainActivity.java
@@ -0,0 +1,68 @@
+package com.aisyahrahmadanipohontu_f55121054.percobaanmodule2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+    private HeroAdapter adapter;
+    private ArrayList<Hero> heroes;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = new HeroAdapter(this);
+        ListView listView = findViewById(R.id.lv_list);
+        listView.setAdapter(adapter);
+
+        prepare();
+        addItem();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int Position, long l) {
+                Toast.makeText(MainActivity.this, heroes.get(Position).getName(), Toast.LENGTH_SHORT)
+                        .show();
+                if (Position == 0) {
+                    startActivity(new Intent(MainActivity.this, TugasAisyah.class));
+                }
+            }
+        });
+    }
+    private void addItem() {
+        heroes = new ArrayList<>();
+
+        for (int i = 0; i < dataName.length; i++) {
+            Hero hero = new Hero();
+            hero.setPhoto(dataPhoto.getResourceId(i, -1));
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+            heroes.add(hero);
+        }
+        adapter.setHeroes(heroes);
+
+    }
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+    }
+}
---
+++ app/src/main/java/com/aisyahrahmadanipohontu_f55121054/percobaanmodule2/TugasAisyah.java
@@ -0,0 +1,14 @@
+package com.aisyahrahmadanipohontu_f55121054.percobaanmodule2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TugasAisyah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tugas_aisyah);
+    }
+}
